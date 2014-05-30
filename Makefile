3e78efb87fe3c2593bd3b381e9a5265bdaf2d499
zanton <huynh@eidos.ic.i.u-tokyo.ac.jp>
2014-05-31 01:30:48 +0900

    [minor] adjusted compile commands further

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+#username?=huynh
+username?=zanton
+
 all:
-	gcc -g -o dagviz dagviz.c read.c layout.c draw.c utils.c print.c -I/home/zanton/parallel2/sys/inst/g/include -L/home/zanton/parallel2/sys/inst/g/lib -Wl,-R/home/zanton/parallel2/sys/inst/g/lib `pkg-config --cflags --libs gtk+-3.0` -ldr -lm
+	gcc -g -o dagviz dagviz.c read.c layout.c draw.c utils.c print.c -I/home/$(username)/parallel2/sys/inst/g/include -L/home/$(username)/parallel2/sys/inst/g/lib -Wl,-R/home/$(username)/parallel2/sys/inst/g/lib `pkg-config --cflags --libs gtk+-3.0` -ldr -lm
--- dagviz.mk
+++ dagviz.mk
@@ -1,6 +1,9 @@
 platform?=g
 prefix?=$(realpath ../../..)/inst/$(platform)
 
+#username?=huynh
+username?=zanton
+
 all: compile
 download :
 compile : compile_done
@@ -12,7 +15,7 @@ compile_done : $(exe)
 	touch $@
 
 $(exe) : dagviz.c
-	gcc -Wall -g -I/home/zanton/parallel2/sys/inst/g/include -L/home/zanton/parallel2/sys/inst/g/lib dagviz.c read.c layout.c draw.c utils.c print.c -Wl,-R/home/zanton/parallel2/sys/inst/g/lib `pkg-config --cflags --libs gtk+-3.0` -o $(exe) -ldr -lm
+	gcc -Wall -g -I/home/$(username)/parallel2/sys/inst/g/include -L/home/$(username)/parallel2/sys/inst/g/lib dagviz.c read.c layout.c draw.c utils.c print.c -Wl,-R/home/$(username)/parallel2/sys/inst/g/lib `pkg-config --cflags --libs gtk+-3.0` -o $(exe) -ldr -lm
 
 install_done: compile_done
 	mkdir -p $(prefix)/bin
