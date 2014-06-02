792551e0bac397564b4d6fcc43cb682956fbe096
zanton <huynh@eidos.ic.i.u-tokyo.ac.jp>
2014-06-02 17:07:52 +0900

    updated README and Makefile (no longer need to change username in Makefile)

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Visualizer for computational DAG of task parallel executions
 Example:
   make
   ./dagviz data/fib4.dag
+
+Prerequisites
+======
+
+You need to have libgtk.  Otherwise the compilation will fail with missing gtk/gtk.h 
+
+  Ubuntu 14.04: libgtk-3-dev package
--- dagviz.mk
+++ dagviz.mk
@@ -2,7 +2,8 @@ platform?=g
 prefix?=$(realpath ../../..)/inst/$(platform)
 
 #username?=huynh
-username?=zanton
+#username?=zanton
+parallel2_dir:=$(prefix)
 
 all: compile
 download :
@@ -14,8 +15,9 @@ exe:=dagviz
 compile_done : $(exe)
 	touch $@
 
+# /home/$(username)/parallel2/sys/inst/g
 $(exe) : dagviz.c
-	gcc -Wall -g -I/home/$(username)/parallel2/sys/inst/g/include -L/home/$(username)/parallel2/sys/inst/g/lib dagviz.c read.c layout.c draw.c utils.c print.c -Wl,-R/home/$(username)/parallel2/sys/inst/g/lib `pkg-config --cflags --libs gtk+-3.0` -o $(exe) -ldr -lm
+	gcc -Wall -g -I$(parallel2_dir)/include dagviz.c read.c layout.c draw.c utils.c print.c -L$(parallel2_dir)/lib -Wl,-R$(parallel2_dir)/lib `pkg-config --cflags --libs gtk+-3.0` -o $(exe) -ldr -lm
 
 install_done: compile_done
 	mkdir -p $(prefix)/bin
